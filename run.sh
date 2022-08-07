6236b1d9f04b62e63d447e686b5bf02079db0b90
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-08-08 01:22:41 +0800

    New
    New

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-rm -rf HinataNew && git clone https://github.com/AyGemuy/HinataNew && sudo apt install ffmpeg imagemagick -y && cd HinataNew && npm install && npm install -g pm2 && pm2 start index.js && pm2 logs
+rm -rf HinataBot && git clone https://github.com/AyGemuy/HinataBot && sudo apt install ffmpeg imagemagick -y && cd HinataBot && npm install && npm install -g pm2 && pm2 start index.js && pm2 logs
