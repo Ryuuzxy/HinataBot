ed6165d14aa3654cad771deaa549b4fc37e58f0f
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-08-13 15:16:36 +0800

    New
    New

--- plugins/update.js
+++ plugins/update.js
@@ -1,8 +1,9 @@
 import { execSync } from 'child_process'
 import fs from 'fs'
 let handler = async (m, { conn, text, isROwner }) => {
+if (!text) throw `Masukkan Username Github Bot Ini`
   if (global.conn.user.jid == conn.user.jid) {
-    let stdout = execSync('git remote set-url origin https://github.com/AyGemuy/HinataNew.git && git pull' + (isROwner && text ? ' ' + text : ''))
+    let stdout = execSync('git remote set-url origin https://github.com/AyGemuy/' + text + ' ' + '&& git pull' + (isROwner && text ? ' ' + text : ''))
     if (isROwner) fs.readdirSync('plugins').map(v => global.reload('', v))
     m.reply(stdout.toString())
   }
